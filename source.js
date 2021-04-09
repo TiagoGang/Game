1be98919b9b0eb1a071fa6973a645a2d2961eeb1
Hac3 <tiagomartinho2@gmail.com>
2021-04-09 09:30:29 +0100

    JavaScipt
    Aprender java

---
+++ source.js
@@ -0,0 +1,15 @@
+let writeOutput = function(text)
+{
+    document.write(<h1>${text}</h1>);
+}
+
+writeOutput("Done");
+
+Let dog = {
+name: "Snoopy",
+numLegs: 4,
+sayNumLegs: function() {
+    return " This dog has " + dog.numLegs + " Legs"
+}
+
+}
